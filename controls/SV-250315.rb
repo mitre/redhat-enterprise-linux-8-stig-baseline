488d9d33459c2ef756fe0252d6df674d1e612f99
Aaron Lippold <lippold@gmail.com>
2025-02-06 00:44:34 -0500

    updated controls to use the os.version.minor for clarity
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- controls/SV-250315.rb
+++ controls/SV-250315.rb
@@ -70,11 +70,11 @@ control 'SV-250315' do
   tag 'host'
 
   message = <<~MESSAGE
-    \nThis check only applies to RHEL versions 8.0 or 8.1.\n
+    \n\nThis check only applies to RHEL versions 8.0 or 8.1.\n
     The system is running RHEL version: #{os.version}, this requirement is Not Applicable.
   MESSAGE
   only_if(message, impact: 0.0) do
-    ['8.0', '8.1'].include?(os.version)
+    os.version.minor.between?(0, 1)
   end
 
   if virtualization.system.eql?('docker')
