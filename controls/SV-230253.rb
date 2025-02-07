a2fdc52b275fe80cc3e7bf12106ef38621391cc4
Aaron Lippold <lippold@gmail.com>
2025-02-06 22:40:05 -0500

    updated the RNG resource, and made a small tweek to the test for RNG to cover the file doesn't exist case
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- controls/SV-230253.rb
+++ controls/SV-230253.rb
@@ -1,6 +1,6 @@
-control 'SV-230253' do
-  title 'RHEL 8 must ensure the SSH server uses strong entropy.'
-  desc 'The most important characteristic of a random number generator is its
+control "SV-230253" do
+  title "RHEL 8 must ensure the SSH server uses strong entropy."
+  desc "The most important characteristic of a random number generator is its
 randomness, namely its ability to deliver random numbers that are impossible to
 predict.  Entropy in computer security is associated with the unpredictability
 of a source of randomness.  The random source with high entropy tends to
@@ -12,8 +12,9 @@ use high-entropy sources by default.  By using the SSH_USE_STRONG_RNG
 environment variable the OPENSSL random generator is reseeded from /dev/random.
  This setting is not recommended on computers without the hardware random
 generator because insufficient entropy causes the connection to be blocked
-until enough entropy is available.'
-  desc 'check', 'Verify the operating system SSH server uses strong entropy with the
+until enough entropy is available."
+  desc "check",
+       'Verify the operating system SSH server uses strong entropy with the
 following command:
 
     Note: If the operating system is RHEL versions 8.0 or 8.1, this requirement
@@ -25,7 +26,8 @@ is not applicable.
 
     If the "SSH_USE_STRONG_RNG" line does not equal "32", is commented out
 or missing, this is a finding.'
-  desc 'fix', 'Configure the operating system SSH server to use strong entropy.
+  desc "fix",
+       'Configure the operating system SSH server to use strong entropy.
 
 Add or modify the following line in the "/etc/sysconfig/sshd" file.
 
@@ -33,34 +35,41 @@ SSH_USE_STRONG_RNG=32
 
 The SSH service must be restarted for changes to take effect.'
   impact 0.3
-  ref 'DPMS Target Red Hat Enterprise Linux 8'
-  tag severity: 'low'
-  tag gtitle: 'SRG-OS-000480-GPOS-00227'
-  tag gid: 'V-230253'
-  tag rid: 'SV-230253r627750_rule'
-  tag stig_id: 'RHEL-08-010292'
-  tag fix_id: 'F-32897r567506_fix'
-  tag cci: ['CCI-000366']
-  tag nist: ['CM-6 b']
-  tag 'host'
-  tag 'container-conditional'
+  ref "DPMS Target Red Hat Enterprise Linux 8"
+  tag severity: "low"
+  tag gtitle: "SRG-OS-000480-GPOS-00227"
+  tag gid: "V-230253"
+  tag rid: "SV-230253r627750_rule"
+  tag stig_id: "RHEL-08-010292"
+  tag fix_id: "F-32897r567506_fix"
+  tag cci: ["CCI-000366"]
+  tag nist: ["CM-6 b"]
+  tag "host"
+  tag "container-conditional"
 
-  only_if('Control not applicable - SSH is not installed within containerized RHEL', impact: 0.0) {
-    !(virtualization.system.eql?('docker') && !file('/etc/sysconfig/sshd').exist?)
-  }
+  only_if(
+    "Control not applicable - SSH is not installed within containerized RHEL",
+    impact: 0.0
+  ) do
+    !(
+      virtualization.system.eql?("docker") &&
+        !file("/etc/sysconfig/sshd").exist?
+    )
+  end
 
-  file = '/etc/sysconfig/sshd'
-  parameter = 'SSH_USE_STRONG_RNG'
-  value = '32'
+  file = "/etc/sysconfig/sshd"
+  parameter = "SSH_USE_STRONG_RNG"
+  value = "32"
   results = parse_config_file(file).params[parameter].to_i
 
-  if random_number_generator.is_software?
+  if random_number_generator.software?
     impact 0.0
-    desc 'justification', "The SSH_USE_STRONG_RNG setting relies on a hardware-based random number generator (HRNG) for sufficient entropy.
+    desc "justification",
+         "The SSH_USE_STRONG_RNG setting relies on a hardware-based random number generator (HRNG) for sufficient entropy.
                           This system lacks a Hardware Random Number Generator (HRNG), causing possible issues with the connection stability."
 
-    describe 'This control is Not Applicable as the SSH server is not using a hardware random number generator.' do
-      skip 'This control is not applicable as the SSH server is not using a hardware random number generator.'
+    describe "This control is Not Applicable as the SSH server is not using a hardware random number generator." do
+      skip "This control is not applicable as the SSH server is not using a hardware random number generator."
     end
   elsif os.version.minor.between?(0, 1)
     message = <<~MESSAGE
@@ -73,9 +82,13 @@ The SSH service must be restarted for changes to take effect.'
       skip message
     end
   else
-    describe 'The SSH server must ensure it uses strong entropy' do
+    describe file(file) do
+      it { should exist }
+    end
+    describe "The SSH server must ensure it uses strong entropy" do
       it "via the '#{parameter}' setting" do
-        expect(results).to cmp(32), "The #{parameter} in the #{file} should be set to #{value}, it is set to #{results}."
+        expect(results).to cmp(32),
+        "The #{parameter} in the #{file} should be set to #{value}, it is set to #{results}."
       end
     end
   end
